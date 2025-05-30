4999714fb083eaa289593ff3292352ead7f314e1
noobless <hakleuka2013@yandex.ru>
2025-05-30 11:33:36 +0500

    completed 1 review

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,16 +2,60 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useRef, useState } from 'react';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
-export const ArticleParamsForm = () => {
+type formProps = {
+	children: React.ReactNode;
+	onApply?: () => void;
+	onReset?: () => void;
+};
+
+export const ArticleParamsForm = ({
+	children,
+	onApply,
+	onReset,
+}: formProps) => {
+	const [isOpen, setOpen] = useState(false);
+	const formRef = useRef<HTMLDivElement>(null);
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef: formRef,
+		onClose: () => setOpen(false),
+		onChange: setOpen,
+	});
+	const containerClass = isOpen
+		? `${styles.container} ${styles.container_open}`
+		: styles.container;
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setOpen((prev) => !prev);
+				}}
+			/>
+			<aside className={containerClass} ref={formRef}>
+				<form className={styles.form} onSubmit={(e) => e.preventDefault()}>
+					{children}
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={() => {
+								onReset?.();
+							}}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={() => {
+								onApply?.();
+							}}
+						/>
 					</div>
 				</form>
 			</aside>
